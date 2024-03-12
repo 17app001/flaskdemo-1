800cc07cad4b73ccde1cc7c4ddebf03f0cacbe19
jerry <jerry@gmail.com>
2024-03-12 19:46:32 +0800

    完成最高最低的數值呈現跟showLoading&hideLoading

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,37 @@
 // 取得主繪製區域
 const myChart = echarts.init(document.getElementById('main'));
+
+
+$("#update").click(() => {
+    drawPM25();
+});
+
+
+
 // 呼叫後端資料跟繪製
 drawPM25();
 
 // 取得後端資料
 function drawPM25() {
+    myChart.showLoading();
     $.ajax(
         {
             url: "/pm25-data",
             type: "GET",
             dataType: "json",
             success: (result) => {
-                //console.log(result);
+                $("#pm25_high_site").text(result["highest"]["site"]);
+                $("#pm25_high_value").text(result["highest"]["pm25"]);
+                $("#pm25_low_site").text(result["lowest"]["site"]);
+                $("#pm25_low_value").text(result["lowest"]["pm25"]);
+
                 //繪製對應區塊並給予必要參數
-                drawChat(myChart, result["datetime"], "PM2.5", result["site"], result["pm25"])
+                drawChat(myChart, result["datetime"], "PM2.5", result["site"], result["pm25"]);
+                myChart.hideLoading();
+            },
+            error: () => {
+                alert("讀取資料失敗，請稍後在試!");
+                myChart.hideLoading();
             }
         }
     )
